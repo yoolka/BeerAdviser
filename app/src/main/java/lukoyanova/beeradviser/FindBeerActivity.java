8289e9548dc64fe62a634617c3f81ca564b7dabe
jlukoyanova <yukos_07@mail.ru>
2019-05-17 17:16:52 +0300

    Initial commit

---
+++ app/src/main/java/lukoyanova/beeradviser/FindBeerActivity.java
@@ -0,0 +1,16 @@
+package lukoyanova.beeradviser;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.Menu;
+import
+
+public class FindBeerActivity extends Activity {
+
+    private BeerExpert expert = new BeerExpert();
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands)
+    }
+}
