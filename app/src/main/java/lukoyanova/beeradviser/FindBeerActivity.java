507b0daacb08b73ff3d08b15684990b52963184f
jlukoyanova <yukos_07@mail.ru>
2019-07-19 18:19:16 +0300

    1

--- app/src/main/java/lukoyanova/beeradviser/FindBeerActivity.java
+++ app/src/main/java/lukoyanova/beeradviser/FindBeerActivity.java
@@ -2,6 +2,7 @@ package lukoyanova.beeradviser;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,7 @@ public class FindBeerActivity extends Activity {
 
     private BeerExpert expert = new BeerExpert();
     public void onClickFindBeer(View view){
-        TextView brands = (TextView) findViewById(R.id.brands)
+        TextView brands = (TextView) findViewById(R.id.);
+        Spinner color = (Spinner) findViewById(R.id.color);
     }
 }
